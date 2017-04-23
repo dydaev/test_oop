ca17d3d2f2ecfaec45098f48bb5df1f867bec10a
dydaev <dydaev@gmail.com>
2017-04-23 15:23:20 +0300

    add in houston rocket plase A12

--- houston.php
+++ houston.php
@@ -7,6 +7,10 @@ require_once 'vendor/autoload.php';
 $c300 = new ArmyRocket('C300');
 $proton = new SpiceRocket('Proton');
 
+$startPlaceA12 = new StartPlace();
 
+//var_dump($c300 instanceof rocketInterface);
+
+$startPlaceA12->addRocket($c300);
 
 
