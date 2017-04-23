a92c089baaca151d4b47549cf735e8dd2829799f
dydaev <dydaev@gmail.com>
2017-04-23 15:06:10 +0300

    add StartPlace.php

---
+++ StartPlace.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace root;
+
+use root\interfaceRocket;
+
+class StartPlace
+{
+	private $rockets = Array();
+	
+	public function addRocket(inatefaceRocket $rocket)
+	{
+		push($this->rockets, $rocket);
+	}
+
+}
