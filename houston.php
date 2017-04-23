8d078585f6bc3953cb5206edae1c77dac7ffa29e
dydaev <dydaev@gmail.com>
2017-04-23 16:49:46 +0300

    rewrite houston and add empty class

--- houston.php
+++ houston.php
@@ -1,16 +1,28 @@
 <?php
-
 namespace root;
 
 require_once 'vendor/autoload.php';
 
-$c300 = new ArmyRocket('C300');
-$proton = new SpiceRocket('Proton');
+use root\rockets\C300;
+use root\rockets\Proton;
+
+class Houston
+{
+	private static $money = 50000000;
+
+
+}
 
 $startPlaceA12 = new StartPlace();
 
-//var_dump($c300 instanceof rocketInterface);
+for($i = 10; $i >= 0; $i--){
+	if($i % 2){
+		$startPlaceA12->addRocket(new C300());
+	}else{
+		$startPlaceA12->addRocket(new Proton());
+	}
+}
 
-$startPlaceA12->addRocket($c300);
-
-
+foreach($startPlaceA12->getStock() as $rocket){
+	echo $rocket->getName()."\n";
+}
