54b297341c48d3f671552fda0986715ae1924d64
dydaev <dydaev@gmail.com>
2017-04-24 11:01:49 +0300

    add payment mathod to StartPlace.php

--- StartPlace.php
+++ StartPlace.php
@@ -7,7 +7,12 @@ use root\rocketInterface;
 class StartPlace
 {
 	private $rockets = Array();
+	private $money;
 	
+	function __construct($money)
+	{
+		$this->money = $money;
+	}
 	public function addRocket(rocketInterface $rocket)
 	{
 		array_push($this->rockets, $rocket);
@@ -17,5 +22,14 @@ class StartPlace
 	{
 		return $this->rockets;
 	}
-
+	
+	public function payment($howMuchMoney)
+	{
+		if ($howMuchMoney <= $this->money) {
+			$this->money -= $howMuchMoney;
+		} else {
+			return null;
+		}
+		return $this->money;
+	}
 }
