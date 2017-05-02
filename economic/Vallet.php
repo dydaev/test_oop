892e8ff6afd19e87990e3f81d525ae2f4747c017
dydaev <dydaev@gmail.com>
2017-05-02 11:15:57 +0300

    changes Vallet.php, rewrite ::clearEmptyMoney

--- economic/Vallet.php
+++ economic/Vallet.php
@@ -7,11 +7,37 @@ abstract class Vallet
 
 	public function putMoney(Money $money)
 	{
-		if (array_key_exists($this->moneys, $money->getCurrency())) {
+		if ($this->checkCurrency($money)) {
 			$this->moneys[$money->getCurrency()]->takeMoney($money);
 		} else {
 			$this->moneys[$money-getCurrency()] = $money;
 		}
 	}
+
+	protected function getMoney($sum, $currecy = 'cvu')
+	{
+		if (array_key_exists($currency, $this->moneys)) {
+			return $this->moneys[$currency]->giveMoney($sum);
+		} else {
+			echo "The vallet dont have this currecy!\n";
+			return null;
+		}
+		$this->clearEmptyMoney();
+	}
+
+	private function clearEmptyMoney()
+	{
+		array_walk($this->moneys, function($currancy, $money) use ($this->money){
+			if ($money->getSum() <= 0) {
+				unset($this->money[$currancy]);
+			}
+		});
+	}
+
+	public function checkCurrency(Money $money)
+	{
+		return array_key_exists($money->getCurrency(),$this->moneys);
+	}
+
 
 }
